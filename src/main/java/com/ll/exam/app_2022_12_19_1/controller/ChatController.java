64896dbe6f46bcafb3b664896d57aa21dcbd8961
minkyoung kim <minry20415@naver.com>
2022-12-19 21:18:11 +0900

    GET /chat/messages?fromId=3 -> 작성한 메세지 리스트 중 index가 4부터 메세지리스트.size()까지의 작성리스트만 조회되게 구현

--- src/main/java/com/ll/exam/app_2022_12_19_1/controller/ChatController.java
+++ src/main/java/com/ll/exam/app_2022_12_19_1/controller/ChatController.java
@@ -4,16 +4,20 @@ import com.ll.exam.app_2022_12_19_1.domain.ChatMessage;
 import com.ll.exam.app_2022_12_19_1.domain.RsData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
 
+    // 작성된 메세지를 저장하는 리스트
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
     // 게시글 작성 후 조회 시 id만 응답
@@ -42,20 +46,42 @@ public class ChatController {
                 "메세지가 작성되었습니다.",
                 new WriteMessageResponse(message.getId()));
     }
-
+// ------------------------------------------------------------------------------
     @AllArgsConstructor
     @Getter
     public static class MessagesResponse {
         private final List<ChatMessage> messages;
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class MessagesRequest {
+        private final Long fromId;
+    }
+
     // 작성한 메세지 조회
     // http://localhost:8020/chat/messages
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<MessagesResponse> messages() {
+    public RsData<MessagesResponse> messages(MessagesRequest req) {
+//        log.debug("req : {}", req);
+        List<ChatMessage> messages = chatMessages;
+
+        // IntStream.range(0, messages.size()) 값 중에 req.fromId와 같은 id 값이 존재하면
+        // 다음으로 넘어가고 아니면 -1을 반환해라
+        if (req.fromId != null) {
+            int index = IntStream.range(0, messages.size()) // 0 부터 messages.size() 까지 반복
+                    .filter(i -> chatMessages.get(i).getId()  == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+            // 내가 찾은 index 의 다음 숫자부터 끝까지
+            if (index != -1) {
+                messages = messages.subList(index+1, messages.size());
+            }
+        }
+
         return new RsData<>("S-1",
                 "조회 성공",
-                new MessagesResponse(chatMessages));
+                new MessagesResponse(messages));
     }
 }
